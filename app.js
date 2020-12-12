f87cab7e73d994a375d12999adc429923fdaf765
yopikurniawan <yopikprabowo@gmail.com>
2020-12-12 22:58:43 +0700

    error deploy heroku

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(express.urlencoded({extended:true}))
 app.use(router)
 app.use(errorHandler)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-// if (process.env.NODE_ENV !== 'test') {
-// }
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+}
 module.exports = app
