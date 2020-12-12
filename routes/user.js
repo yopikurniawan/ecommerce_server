9469b67023eb81f7433d714ac0c976975e45e420
yopikurniawan <yopikprabowo@gmail.com>
2020-12-12 22:04:50 +0700

    first

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/user')
 
+router.get('/', (req, res) => {
+  res.send('CMS Server YKP')
+})
 router.post('/login/admin', UserController.loginAdmin)
 router.post('/register', UserController.register)
 router.post('/login/customer', UserController.loginCustomer) 
